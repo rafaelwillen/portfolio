784e2034f01f8b1834799d59d4b5c665e8a5335a
Rafael Willen <rafaelpadre20@gmail.com>
2024-02-27 18:13:30 +0100

    Add colors to the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ export default {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
     extend: {},
+    colors: {
+      primary: {
+        blackPearl: "#01080E",
+        darkBlue: "#011627",
+        midnightBlue: "#011221",
+      },
+      secondary: {
+        steelBlue: "#607B96",
+        lightSeaGreen: "#3C9D93",
+        mediumBlue: "#4D5BCE",
+      },
+      accent: {
+        sandyBrown: "#FEA55F",
+        mediumAquamarine: "#43D9AD",
+        lightCoral: "#E99287",
+        lightPastelPurple: "#C98BDF",
+      },
+      white: "#fff",
+      black: "#000",
+      transparent: "transparent",
+    },
   },
   plugins: [
     // eslint-disable-next-line no-undef
